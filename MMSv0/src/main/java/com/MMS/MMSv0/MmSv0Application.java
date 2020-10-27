9f7b830851c6ba4475e142847132d5a3674a6224
Anoop Nair V <anoopv.edu@gmail.com>
2020-10-27 20:31:16 +0530

    Backend upload

---
+++ MMSv0/src/main/java/com/MMS/MMSv0/MmSv0Application.java
@@ -0,0 +1,13 @@
+package com.MMS.MMSv0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmSv0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MmSv0Application.class, args);
+	}
+
+}
